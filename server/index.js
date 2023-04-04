5cd05f4113876201f776b3745de2fe7e3b9fd965
jaypatel7212 <jaypatel.lamprostech@gmail.com>
2023-04-04 09:58:17 +0530

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,46 +1,71 @@
 const express = require("express");
-const cors = require("cors");
-const crypto = require("./crypto");
-
 const app = express();
+const cors = require("cors");
 const port = 3042;
+
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
 
 app.use(cors());
 app.use(express.json());
 
-const balances = new Map([
-  ["B751181881B238BC03AFF2F05238523E5153CE7C", 100], // bob
-  ["2691DFF0EEAE64F2BF99B775A5E8A6BD0C1D01A3", 50], // alice
-  ["859769B97161E02C983A49D6B3DCCBECE32431E8", 75], // charles
-]);
+const balances = {
+  '3aa677a63fe530e19124': 100,
+  "a3cd6cadf68626f1373a": 50,
+  "2ab494e00e38840b38bc": 75,
+};
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
-  const balance = balances.get(address) || 0;
+  const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post("/send", (req, res) => {
-  const { message, signature } = req.body;
-  const { recipient, amount } = message;
-
-  const pubKey = crypto.signatureToPubKey(message, signature);
-  const sender = crypto.pubKeyToAddress(pubKey);
+app.post("/send", async (req, res) => {
+  const { sender, recipient, amount, nonce, signTxn } = req.body;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances.get(sender) < amount) {
+  // retrieve signature and recovery bit 
+  const [signature, recoveryBit] = signTxn;
+  // convert signature to Uint8Array
+  const formattedSignature = Uint8Array.from(Object.values(signature));
+  //message hash
+  const msgToBytes = utf8ToBytes(recipient + amount + JSON.stringify(nonce));
+  const msgHash = toHex(keccak256(msgToBytes));
+
+  // recover public key
+  const publicKey = await secp.recoverPublicKey(msgHash, formattedSignature, recoveryBit);
+
+  // verify transection 
+  const verifyTx = secp.verify(formattedSignature, msgHash, publicKey)
+
+  if (!verifyTx) {
+    res.status(400).send({ message: "Invalid Transection" });
+  }
+
+  if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances.set(sender, balances.get(sender) - amount);
-    balances.set(recipient, balances.get(recipient) + amount);
-    res.send({ balance: balances.get(sender) });
+  }
+  else if (sender == recipient) {
+    res.status(400).send({ message: "Please! Enter Another address" })
+  }
+  
+  else if(recipient && amount) {
+    balances[sender] -= amount;
+    balances[recipient] += amount;
+    res.send({ balance: balances[sender] });
+  }
+  else {
+    res.status(400).send({ message: "Something Went Wrong !!" })
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  
 });
 
 function setInitialBalance(address) {
