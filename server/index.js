536317566c6c86bf20da5b16f3bcc7f9f08ab73f
jaypatel7212 <jaypatel.lamprostech@gmail.com>
2023-04-04 22:51:48 +0530

    today's commit

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  '3aa677a63fe530e19124': 100,
-  "a3cd6cadf68626f1373a": 50,
-  "2ab494e00e38840b38bc": 75,
+  "04084e3ad2b8f18e9b7b226dc2338459450bae4f8f04398f8e100d3933da43ac840e4cdf1da945dabcf626cdae4a3d504e89eec87d0078aa3c7a0f5b5ada570dbd": 100,
+  "042c0dadbcae32fd745742ef7f8a99e045bd2b53b9c07f3867c8cd776d036ba6ea4a3ce7187bee3cdb6831d9e17a86824f9b55fda70504042ff8bb328db44d3ae6": 50,
+  "041eea52892f79c18d892e85a57166b82a8631e0736ba84a0edc5d03e396d272e4ece9f7c055a531316915b1e4fde13e742c3568499a21e8bbff1c8be5468a065a": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -28,7 +28,7 @@ app.post("/send", async (req, res) => {
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  // retrieve signature and recovery bit 
+  // retrieve signature and recovery bit
   const [signature, recoveryBit] = signTxn;
   // convert signature to Uint8Array
   const formattedSignature = Uint8Array.from(Object.values(signature));
@@ -37,10 +37,14 @@ app.post("/send", async (req, res) => {
   const msgHash = toHex(keccak256(msgToBytes));
 
   // recover public key
-  const publicKey = await secp.recoverPublicKey(msgHash, formattedSignature, recoveryBit);
+  const publicKey = await secp.recoverPublicKey(
+    msgHash,
+    formattedSignature,
+    recoveryBit
+  );
 
-  // verify transection 
-  const verifyTx = secp.verify(formattedSignature, msgHash, publicKey)
+  // verify transection
+  const verifyTx = secp.verify(formattedSignature, msgHash, publicKey);
 
   if (!verifyTx) {
     res.status(400).send({ message: "Invalid Transection" });
@@ -48,24 +52,19 @@ app.post("/send", async (req, res) => {
 
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
-  }
-  else if (sender == recipient) {
-    res.status(400).send({ message: "Please! Enter Another address" })
-  }
-  
-  else if(recipient && amount) {
+  } else if (sender == recipient) {
+    res.status(400).send({ message: "Please! Enter Another address" });
+  } else if (recipient && amount) {
     balances[sender] -= amount;
     balances[recipient] += amount;
     res.send({ balance: balances[sender] });
-  }
-  else {
-    res.status(400).send({ message: "Something Went Wrong !!" })
+  } else {
+    res.status(400).send({ message: "Something Went Wrong !!" });
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
-  
 });
 
 function setInitialBalance(address) {
